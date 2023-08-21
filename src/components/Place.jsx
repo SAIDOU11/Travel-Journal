f68616075762cb743b0bac6410261be65008ebaf
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-21 10:12:01 +0200

    travel journal

--- src/components/Place.jsx
+++ src/components/Place.jsx
@@ -9,11 +9,33 @@ const Place = (props) => {
     imageUrl,
   } = props;
   return (
-    <div>
-      <div className="img">
-        <img src={imageUrl} alt={title} />
-      </div>
-    </div>
+    <>
+      <div className="container">
+        <div className="img">
+          <img src={imageUrl} alt={title} />
+        </div>
+        <div className="info-container">
+          <div className="place">
+            <p>
+              <i
+                style={{ fontSize: '1.1rem', color: '#F55A5A' }}
+                class="fa-solid fa-location-dot"
+              ></i>
+            </p>{' '}
+            <p className="location">{location}</p>
+            <p className="google-map">View on Google Maps</p>
+          </div>
+          <div className="info-place">
+            <h1>{title} </h1>
+            <p className="date">
+              {startDate} - {endDate}
+            </p>
+            <p className="description">{description}</p>
+          </div>
+        </div>{' '}
+      </div>{' '}
+      <hr className="hr-line" />
+    </>
   );
 };
 
